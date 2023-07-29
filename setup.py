2f31b72ed447643140f7003430b26273290239f4
Prashanth Katakam <prashanth01071995@gmail.com>
2023-07-29 12:42:37 +0530

    updated setup file to install required libraries

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ with open('requirements.txt', 'r') as f:
     required_libraries = f.read().splitlines()
 
 setup(
-    name='Galaxy',  # Replace with your project name
-    version='0.1.0',  # Replace with the desired version number
-    description='Description of your project',
+    name='Galaxy',  
+    version='0.1.0',  
+    description='Description of your project',# Write a proper description 
     author='Prashanth',
     author_email='prashanth01071995@gmail.com',
     packages=find_packages(),
     install_requires=required_libraries,
-    # Add other metadata such as license, url, etc. if needed
 )
